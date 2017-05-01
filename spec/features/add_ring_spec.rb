b78c55f95a151fa846e45f9e0ad37cd1962c34a7
Andrew Rady <andrew.arsoftware@gmail.com>
2017-05-01 00:23:44 -0600

    tested for rings_new_path form

--- spec/features/add_ring_spec.rb
+++ spec/features/add_ring_spec.rb
@@ -8,4 +8,46 @@ RSpec.feature 'add ring' do
 	end
 
 
+
+	scenario 'add ring number' do
+		visit '/rings/new'
+
+		fill_in 'Ring number', with: '1'	
+	
+		click_button('Submit')
+
+		expect(page).to have_content('1')
+	end
+
+	scenario 'add ring rank' do
+		visit 'rings/new'
+
+		fill_in 'Ring rank', with: 'white-yellow'
+
+		click_button('Submit')
+
+		expect(page).to have_content('white-yellow')
+	end
+	
+	scenario 'add ring age' do
+		visit 'rings/new'
+
+		fill_in 'Ring age', with: '18-29'
+
+		click_button('Submit')
+
+		expect(page).to have_content('18-29')
+	end
+
+	scenario 'add ring gender' do
+		visit 'rings/new'
+
+		fill_in 'Ring gender', with: 'Men'
+
+		click_button('Submit')
+
+		expect(page).to have_content('Men')
+	end
+
+
 end
