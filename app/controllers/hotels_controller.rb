6f77e8e42f17d32e8a5292bad433756f39afb908
Andrew Rady <andrew.arsoftware@gmail.com>
2017-06-10 00:39:16 -0600

    added stying to hotel form and destroy action

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -5,6 +5,14 @@ class HotelsController < ApplicationController
 		redirect_to event_path(@event)
 	end
 
+  def destroy
+		@event = Event.find(params[:event_id])
+		@hotel = @event.hotels.find(params[:id])
+		@hotel.destroy
+
+		redirect_to event_path(@event)
+  end
+
 	private
 		def hotel_params
 			params.require(:hotel).permit(:name, :dates, :cost, :link, :number, :address)
