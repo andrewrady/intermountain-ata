f2a3c13390c7bafd1c44f46ce4a79d2184413959
Andrew Rady <andrew.arsoftware@gmail.com>
2017-06-11 21:00:43 -0600

    added delete feature for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,6 +34,13 @@ class EventsController < ApplicationController
 		end
   end
 
+	def destroy
+		@event = Event.find(params[:id])
+
+		@event.destroy
+		redirect_to rings_path
+	end
+
 
   private
     def event_params
